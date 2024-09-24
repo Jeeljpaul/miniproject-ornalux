9e0e88f31735be3bf7e4ed67bb1d57a124da6779
jeeljpaul <jeeljpaul2025@mca.ajce.in>
2024-09-24 07:24:22 +0530

    product_management

--- jewelryapp/urls.py
+++ jewelryapp/urls.py
@@ -14,9 +14,13 @@ urlpatterns = [
     path('forgot_password/', views.forgot_password),
     path('reset-password/<str:token>/', views.reset_password),
     path('adminhome/add_product/', views.add_product),
-    path('adminhome/view_products/',views.view_products),
-    path('view_product_details/<int:product_id>/', views.view_product_details),
-    path('delete_product/<int:product_id>/', views.delete_product),
+    path('adminhome/view_products/',views.view_products, name='view_products'),
+    path('view_product_details/<int:product_id>/', views.view_product_details, name='view_product_details'),
+    path('delete_product/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('update_product/<int:product_id>/', views.update_product, name='update_product'),
+    path('view_registered_users/', views.view_registered_users,),
+    path('delete_user/<int:user_id>/', views.delete_user, name='delete_user'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
